8b01accadb84c79297b88afcf675276ee7712ccc
jennyxiaqc <xiayun78@hotmail.com>
2020-03-07 16:22:43 -0500

    1[NEW] Add ProductController,using property inject

---
+++ Vic.SportStore/Vic.SportStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SuperStore.Domain.Abstract;
+
+namespace Vic.SportStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository ProductsReposotory { get; set; }   //Property zhu RU
+
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(ProductsReposotory.Products);
+        }
+    }
+}
