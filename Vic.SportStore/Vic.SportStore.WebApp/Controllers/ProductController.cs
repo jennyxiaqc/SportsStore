53387090c391bf9ceae6c604e28474102fe4a3a7
jennyxiaqc <xiayun78@hotmail.com>
2020-03-21 16:02:21 -0400

    1[NEW] Add page info, Add Model,revise controller, revise web.config in WebApp, r

--- Vic.SportStore/Vic.SportStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportStore/Vic.SportStore.WebApp/Controllers/ProductController.cs
@@ -4,17 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vic.SportStore.Domain.Abstract;
+using Vic.SportStore.WebApp.Models;
 
 namespace Vic.SportStore.WebApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IProductsRepository ProductsReposotory { get; set; }   //Property inject
+        public int PageSize = 2;
+        public IProductsRepository ProductsRepository { get; set; }   //Property inject
 
         // GET: Product
-        public ViewResult List()
+        //public ViewResult List()
+        //{
+        //    return View(ProductsRepository.Products);
+        //}
+
+
+        public ViewResult List(int page = 1)
         {
-            return View(ProductsReposotory.Products);
+            //return View(
+            //ProductsRepository
+            //.Products
+            //.OrderBy(p => p.ProductId)
+            //.Skip((page - 1) * PageSize)
+            //.Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
+                .Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
+            return View(model);
+
         }
     }
 }
